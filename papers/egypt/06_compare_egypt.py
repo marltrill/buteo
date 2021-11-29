ca52bbd8637ffd787e9ea61b253852182e5aa682
marltrill <marciatrillo@gmail.com>
2021-11-29 13:11:00 +0100

    update names

--- papers/egypt/06_compare_egypt.py
+++ papers/egypt/06_compare_egypt.py
@@ -25,6 +25,10 @@ def round_to_decimals(x):
     return f"{np.round(x, 4):.4f}"
 
 
+def round_to_percent(x):
+    return f"{np.round(x, 2):.2f} %"
+
+
 def metrics(truth, pred, name, resample=False, target=None):
     if not isinstance(truth, list):
         truth = [truth]
@@ -70,14 +74,54 @@ def metrics(truth, pred, name, resample=False, target=None):
 
     mae = round_to_decimals(mean_absolute_error(tarr, parr))
     mse = round_to_decimals(mean_squared_error(tarr, parr))
-    tpe = round_to_decimals(((np.sum(parr) - np.sum(tarr)) / np.sum(tarr)) * 100)
+    rsme = round_to_decimals(mean_squared_error(tarr, parr, squared=False))
+    tpe = round_to_percent(((np.sum(parr) - np.sum(tarr)) / np.sum(tarr)) * 100)
 
-    if target == "people": #"area" ?
-        tarr = np.array(tarr > 0.01, dtype=np.uint8)
-        parr = np.array(parr > 0.01, dtype=np.uint8)
+    if target == "people":
+        tarr_mask = tarr > 0.01
+        parr_mask = parr > 0.01
+        tarr = np.array(tarr_mask, dtype=np.uint8)
+        parr = np.array(parr_mask, dtype=np.uint8)
     else:
-        tarr = np.array(tarr >= 1.0, dtype=np.uint8)
-        parr = np.array(parr >= 1.0, dtype=np.uint8)
+        tarr_mask = tarr > 1.0
+        parr_mask = parr > 1.0
+        tarr = np.array(tarr_mask, dtype=np.uint8)
+        parr = np.array(parr_mask, dtype=np.uint8)
+
+    tarr_masked_pred = tarr[parr_mask].astype("float32")
+    parr_masked_pred = parr[parr_mask].astype("float32")
+    tarr_masked_truth = tarr[tarr_mask].astype("float32")
+    parr_masked_truth = parr[tarr_mask].astype("float32")
+
+    mae_pos = round_to_decimals(mean_absolute_error(tarr_masked_pred, parr_masked_pred))
+    mse_pos = round_to_decimals(mean_squared_error(tarr_masked_pred, parr_masked_pred))
+    rsme_pos = round_to_decimals(
+        mean_squared_error(tarr_masked_pred, parr_masked_pred, squared=False)
+    )
+    tpe_pos = round_to_percent(
+        (
+            (np.sum(parr_masked_pred) - np.sum(tarr_masked_pred))
+            / np.sum(tarr_masked_pred)
+        )
+        * 100
+    )
+
+    mae_neg = round_to_decimals(
+        mean_absolute_error(tarr_masked_truth, parr_masked_truth)
+    )
+    mse_neg = round_to_decimals(
+        mean_squared_error(tarr_masked_truth, parr_masked_truth)
+    )
+    rsme_neg = round_to_decimals(
+        mean_squared_error(tarr_masked_truth, parr_masked_truth, squared=False)
+    )
+    tpe_neg = round_to_percent(
+        (
+            (np.sum(parr_masked_truth) - np.sum(tarr_masked_truth))
+            / np.sum(tarr_masked_truth)
+        )
+        * 100
+    )
 
     acc = round_to_decimals(accuracy_score(tarr, parr))
     bacc = round_to_decimals(balanced_accuracy_score(tarr, parr))
@@ -87,12 +131,21 @@ def metrics(truth, pred, name, resample=False, target=None):
 
     adjust_name = name.ljust(10, " ")
 
-    print(f"{adjust_name} (reg) - MAE: {mae}, MSE: {mse}, TPE: {tpe}")
     print(
-        f"{adjust_name} (bin) - ACC: {acc}, BACC: {bacc}, PREC: {prec}, REC: {rec}, F1: {f1}"
+        f"{adjust_name} (regresion) - MAE: {mae}, MSE: {mse}, RMSE: {rsme}, TPE: {tpe}"
+    )
+    print(
+        f"{adjust_name} (reg_pred)  - MAE: {mae_pos}, MSE: {mse_pos}, RMSE: {rsme_pos}, TPE: {tpe_pos}"
+    )
+    print(
+        f"{adjust_name} (reg_true)  - MAE: {mae_neg}, MSE: {mse_neg}, RMSE: {rsme_neg}, TPE: {tpe_neg}"
+    )
+    print(
+        f"{adjust_name} (binary)    - ACC: {acc}, BACC: {bacc}, PREC: {prec}, REC: {rec}, F1: {f1}\n"
     )
 
-target= "area"
+
+target = "area"
 resample = True
 
 base_truth= "D:/NIRAS/pilot_test/vector_data/rasterized_aa/Aligned/"
@@ -103,21 +156,22 @@ base_predictions = "C:/Users/MALT/Desktop/set3_patches/predictions_pilotarea/Ali
 pred_alexandria11 = base_predictions + f"11aa_prediction_{target}.tif"
 pred_alexandria12 = base_predictions + f"12aa_prediction_{target}.tif"
 
-# Are rasters aligned?
-base_truth_11aa= "D:/NIRAS/pilot_test/vector_data/rasterized_aa/Aligned/11aa/"
-images = glob(base_truth_11aa + "*.tif")
-rasters_are_aligned(images, same_extent=False, same_dtype=False, same_nodata=False)
+# # Are rasters aligned?
+# base_truth_11aa= "D:/NIRAS/pilot_test/vector_data/rasterized_aa/Aligned/11aa/"
+# images = glob(base_truth_11aa + "*.tif")
+# rasters_are_aligned(images, same_extent=False, same_dtype=False, same_nodata=False)
 
-base_truth_12aa= "D:/NIRAS/pilot_test/vector_data/rasterized_aa/Aligned/12aa/"
-images = glob(base_truth_12aa + "*.tif")
-rasters_are_aligned(images, same_extent=False, same_dtype=False, same_nodata=False)
+# base_truth_12aa= "D:/NIRAS/pilot_test/vector_data/rasterized_aa/Aligned/12aa/"
+# images = glob(base_truth_12aa + "*.tif")
+# rasters_are_aligned(images, same_extent=False, same_dtype=False, same_nodata=False)
 
-# %%
+if resample:
+    print("Pixel_size = 100")
+else:
+    print("Pixel_size = 10")
 
-#Pixel Size= 10m (We are not resampling)
-resample = False #If we want to resample to 100m pixels, set resample= True
-metrics(truth_alexandria11, pred_alexandria11, "Site 11a", resample=resample, target=target)
-metrics(truth_alexandria12, pred_alexandria12, "Site 12a", resample=resample, target=target)
+metrics(truth_alexandria11, pred_alexandria11, "Site 11", resample=resample, target=target)
+metrics(truth_alexandria12, pred_alexandria12, "Site 12", resample=resample, target=target)
 metrics(
     [
         truth_alexandria11,
@@ -128,33 +182,8 @@ metrics(
         pred_alexandria12,
     ],
     "All",
-    resample=resample, #test sites not combined: resample= resample
-    target=target, #test sites not combined: target= target, or "area", its the same
+    resample=resample,
+    target=target,
 )
 
 # %%
-
-# if target == "area":
-#     print("")
-#     truth_odense = folder + f"odense_label_{target}.tif"
-#     truth_bornholm = folder + f"bornholm_label_{target}.tif"
-
-#     pred_odense = folder + f"odense_prediction_{target}.tif"
-#     pred_bornholm = folder + f"bornholm_prediction_{target}.tif"
-
-#     metrics(truth_odense, pred_odense, "Odense", resample=resample, target=target)
-#     metrics(truth_bornholm, pred_bornholm, "Bornholm", resample=resample, target=target)
-#     metrics(
-#         [
-#             truth_odense,
-#             truth_bornholm,
-#         ],
-#         [
-#             pred_odense,
-#             pred_bornholm,
-#         ],
-#         "All",
-#         resample= resample, 
-#         target=target, 
-#     )
-
